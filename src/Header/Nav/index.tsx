1b034c4dca77d6047f0abe1e2e4130970d5748da
David Arrizon <arrizon.david@gmail.com>
2026-06-01 03:37:32 -0700

    added blocks for form components, added pagination component, added media components, added heros, added tests for frontend, added types for media, added pagination ui component, added storefront icons, updated nav and email form

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -12,13 +12,20 @@ export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
   const navItems = data?.navItems || []
 
   return (
-    <nav className="flex gap-3 items-center">
+    <nav
+      aria-label="Main Navigation"
+      className="flex flex-wrap items-center justify-end gap-x-4 gap-y-2 text-sm md:text-base"
+    >
       {navItems.map(({ link }, i) => {
         return <CMSLink key={i} {...link} appearance="link" />
       })}
-      <Link href="/search">
+      <Link
+        aria-label="Search"
+        className="inline-flex size-8 items-center justify-center rounded-md hover:bg-primary/10 focus-visible:outline-1 focus-visible:ring-4 focus-visible:ring-ring/20"
+        href="/search"
+      >
         <span className="sr-only">Search</span>
-        <SearchIcon className="w-5 text-primary" />
+        <SearchIcon aria-hidden="true" className="w-5 text-primary" />
       </Link>
     </nav>
   )
